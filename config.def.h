0e3f2c67670f48d50e7a3ba74a8691c8db2e627c
ZK <72352225+africavoid@users.noreply.github.com>
2023-05-30 21:28:03 +0100

    My fork of tabbed

--- config.def.h
+++ config.def.h
@@ -36,18 +36,12 @@ static Bool npisrelative  = False;
 #define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY,     XK_Return, spawn,       { 0 } },
-
+	{ MODKEY,     XK_space, focusonce,   { 0 } },
+	{ MODKEY,     XK_space, spawn,       { 0 } },
 	{ MODKEY,     XK_l,      rotate,      { .i = +1 } },
 	{ MODKEY,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY,     XK_j,	 rotate,      { .i = -1 } },
-	{ MODKEY,     XK_k,	 rotate,      { .i = +1 } }, 	
-	//{ MODKEY,     XK_j,      movetab,     { .i = -1 } },
-	//{ MODKEY,     XK_k,      movetab,     { .i = +1 } },
 	{ MODKEY,     XK_Tab,    rotate,      { .i = 0 } },
-
-	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
-	{ 0,                    XK_F11,    fullscreen,  { 0 } },
+	{ MODKEY,     XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
+	{ MODKEY,     XK_k,      killclient,  { 0 } },
+	{ 0,          XK_F11,    fullscreen,  { 0 } },
 };
