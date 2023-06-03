4ad095da1da62fdd905c50fb84ac502b33ead71c
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-06-03 20:27:56 +0100

    No overlapping keys

--- config.def.h
+++ config.def.h
@@ -38,8 +38,8 @@ static Key keys[] = {
 	/* modifier             key        function     argument */
 	{ MODKEY,     XK_space, focusonce,   { 0 } },
 	{ MODKEY,     XK_space, spawn,       { 0 } },
-	{ MODKEY,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY,     XK_h,      rotate,      { .i = -1 } },
+	{ ShiftMask,  XK_l,      rotate,      { .i = +1 } },
+	{ ShiftMask,  XK_h,      rotate,      { .i = -1 } },
 	{ MODKEY,     XK_Tab,    rotate,      { .i = 0 } },
 	{ MODKEY,     XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
 	{ MODKEY,     XK_q,      killclient,  { 0 } },
